156c29c28542dde9396a48441008e91194b43522
Daryoosh Hoora <daryoosh@hoora.info>
2023-05-09 23:20:38 +0330

    02 complete

--- 02_variables/variables.c
+++ 02_variables/variables.c
@@ -1,26 +1,29 @@
 //------------------------------------------------------------------------------
-// Data types in C:
+// Data types, Variables and Constants.
 //
-// 	- int: 2 or 4 bytes, to store whole numbers, without decimal point
-//	- float: 4 bytes, to store numbers with 6-7 decimal digits
-//	- double: 8 bytes, to store numbers with 15 decimal digits
-//	- char: 1 bytes, to store just a single character as any type which
-//	        surrounded by single quotations
 //------------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#include <stdbool.h>
 
 int main() {
 	// Variable declarations
-	/* 
+    // <data_type> identifier
+    /* 
 	int myInt;
-	float myFloat;
+	short myShort;
+    long myLong;
+    float myFloat;
 	double myDouble;
 	char myChar; 
 	*/
 
 	/* It's a best practice to align block of identifiers by Tab */ 
 	int 	myInt;
+    short   myShort;
+    long    myLong;
 	float 	myFloat;
 	double 	myDouble;
 	char 	myChar;
@@ -29,8 +32,13 @@ int main() {
     int x, y, z;
 
     // Variable definitions
+    // <data_type> identifier = value;
     int a = 7;
     int b = 8, c = 9;
+
+    // Variable initialization
+    int d;
+    d = 1;
 
     // Variable assignments
 	myInt = 5;
@@ -43,10 +51,10 @@ int main() {
     a = 12;
 
 	// Print variables
-	/* printf is an unsafe data type function, so it is necessary to using
-	   format specifier as %x specific for each data type.
-	*/
-	printf("myInt: %i\n", myInt);
+	// printf is an unsafe data type function, so it is necessary to using
+	// format specifier as %x specific for each data type.
+	/*
+    printf("myInt: %i\n", myInt);
     printf("myInt: %d\n", myInt);
     printf("myFloat: %f\n", myFloat);
 	printf("myFloat: %.1f\n", myFloat);
@@ -56,7 +64,57 @@ int main() {
     printf("myDouble: %lf\n", myDouble);
     printf("myDouble: %.9lf\n", myDouble);
     printf("myDouble: %.15lf\n", myDouble);
-	printf("myChar: %c\n", myChar);
+	printf("myChar: %c\n\n", myChar);
+    */
 
-	return 0;
+    //Complex data type variable declaration
+    /*
+    signed   int si;
+    unsigned int usi;
+    */
+    
+    //Constants
+    //Using <const> while declaration a variable
+    const int TRUE = 1;
+    const int FALSE = 0;
+    
+    printf("%d\n", TRUE);
+    printf("%d\n", FALSE);
+
+    //Boolean
+    bool t = true;
+    bool f = false;
+
+    printf("t is %s\n", t ? "True" : "False");
+
+    printf("Data Type            Byte(s) Range\n");
+    printf("--------------------------------------------------------------\n");
+    printf("[signed] char        %d       [%d, %d]\n", CHAR_BIT / 8, 
+            SCHAR_MIN, SCHAR_MAX);
+    printf("unsigned char        %d       [%d, %d]\n", CHAR_BIT / 8, 
+            0, UCHAR_MAX);
+    
+    printf("[signed] short [int] %lu       [%d, %d]\n", sizeof(short), 
+            SHRT_MIN, SHRT_MAX);
+    printf("unsigned short [int] %lu       [%d, %d]\n", sizeof(unsigned short), 
+            0, USHRT_MAX);
+    
+    printf("[signed] [int]       %lu       [%d, %d]\n", sizeof(int), 
+            INT_MIN, INT_MAX);
+    printf("unsigned [int]       %lu       [%d, %u]\n", sizeof(unsigned int), 
+            0, UINT_MAX);
+    
+    printf("[signed] long [int]  %lu       [%ld, %ld]\n", sizeof(long), 
+            LONG_MIN, LONG_MAX);
+    printf("unsigned long [int]  %lu       [%d, %lu]\n", sizeof(unsigned long), 
+            0, ULONG_MAX);
+
+    printf("         float       %lu       [%g, %g]\n", sizeof(float), 
+            -FLT_MAX, FLT_MAX);
+    printf("         double      %lu       [%g, %g]\n", sizeof(double), 
+            -DBL_MAX, DBL_MAX);
+    printf("         long double %lu      [%Lg, %Lg]\n", sizeof(long double), 
+            -LDBL_MAX, LDBL_MAX);
+    
+    return 0;
 }
